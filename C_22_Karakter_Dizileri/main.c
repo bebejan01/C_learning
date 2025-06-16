416767664febe0dfdb7af187ba704333e2abaac1
bebejan01 <mustafademirel047@gmail.com>
2025-06-16 15:41:01 +0300

    Added

---
+++ C_22_Karakter_Dizileri/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+	
+	
+	char isim[] = "Murat";
+	
+	printf("%c\n", isim[1]);
+	
+	
+	char adim[20];
+	
+	printf("Isminizi girin: ");
+	
+	scanf("%s", adim);
+	
+	printf("Isminiz: %s\n", adim);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
