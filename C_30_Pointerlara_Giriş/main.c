85deab3b67392d6e15f230cf9893bf74b9a9a21c
bebejan01 <mustafademirel047@gmail.com>
2025-06-18 17:51:01 +0300

    Addeed

---
+++ C_30_Pointerlara_Giriş/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+int main() {
+	
+	int i = 5;
+	
+	int *p = &i;
+	
+	printf("%u\n", p);
+	
+	// & değişkenin adres bilgisini veriyo.
+	// * adresteki değeri verir.
+	
+	printf("%d", *p);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
