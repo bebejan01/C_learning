34f780ea7e8cdfaaa7e4ab5a1db4d85598444f1f
bebejan01 <mustafademirel047@gmail.com>
2025-07-17 18:43:53 +0300

    Added

---
+++ C_47_Yapıları_Referans_ile_Göndermek_CallByReference/main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <string.h>
+
+struct ogrenci {
+	
+	char isim[20];
+	char soyisim[20];
+	int numara;
+	
+};
+struct ogrenci *degerAl(struct ogrenci *p) {
+	
+	strcpy(p->isim,"Mehmet");
+	strcpy(p->soyisim,"Gencol");
+	p->numara = 455;
+	
+	
+	return p;
+	
+	
+	
+}
+void goster(struct ogrenci *p) {
+	
+	printf("Ogrencinin Bilgileri\nIsim:%s\nSoyisim:%s\nNumara:%d",p->isim,p->soyisim,p->numara);
+}
+
+int main() {
+	
+	struct ogrenci *tut;
+	
+	struct ogrenci ogrenci1 = {"Ahmet", "Gedikli", 505};
+	
+//	goster(&ogrenci1);
+	
+	tut = degerAl(&ogrenci1);
+	
+	printf("%s %s %d", tut->isim,tut->soyisim,tut->numara);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
